42c010ba9638bb9cc5418ee68f3c04612d1dfd0d
Valery Sazonov <vksazonov@gmail.com>
2023-06-08 20:45:09 +0300

    Added env variables, server is working

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import { dbInit } from './utils/dbInit';
 
 const app = express();
 const PORT = 5000;
+
+dbInit();
 
 app.use(cors());
 
---
+++ src/utils/dbInit.ts
@@ -0,0 +1,18 @@
+
+/* eslint-disable max-len */
+import { Sequelize } from 'sequelize';
+require('dotenv').config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URI = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+export const dbInit = () => {
+  return new Sequelize(
+    URI,
+    {
+      dialectOptions: {
+        ssl: true,
+      },
+    },
+  );
+};
