9b518f7bfe50a051710968022575c1919f5c7013
Goto-Light <vadim.tkachenko.1997@gmail.com>
2023-06-08 19:42:41 +0300

    add cors and fix intstalls in package.json

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
 
 app.get('/', (_req, res) => {
   res.send('Hello world');
