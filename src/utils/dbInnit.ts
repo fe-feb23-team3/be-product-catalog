8d1a71664c0fa18e43b81d3979c87328f2619cae
Valery Sazonov <vksazonov@gmail.com>
2023-06-08 17:31:53 +0300

    Added Sequelize

---
+++ src/utils/dbInnit.ts
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+import { Sequelize } from 'sequelize';
+
+const URI = `postgres://${process.env.DB_NAME}:${process.env.DB_PASSWORD}-long-meadow-678376.us-east-2.aws.neon.tech/neondb`;
+
+export const bdInit = () => {
+  return new Sequelize(
+    URI,
+    {
+      dialectOptions: {
+        ssl: true,
+      },
+    },
+  );
+};
