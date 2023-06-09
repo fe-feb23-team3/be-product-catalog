4d530795680648f6556c75ccceeefe8d1c9d207c
Valery Sazonov <114118243+vksazonov@users.noreply.github.com>
2023-06-09 17:07:05 +0300

    Created prod table/ pushed to neon (#7)

--- build/server.js
+++ build/server.js
@@ -4,8 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dbInit_1 = require("./utils/dbInit");
 const app = (0, express_1.default)();
 const PORT = 5000;
+(0, dbInit_1.dbInit)();
+app.use((0, cors_1.default)());
 app.get('/', (_req, res) => {
     res.send('Hello world');
 });
