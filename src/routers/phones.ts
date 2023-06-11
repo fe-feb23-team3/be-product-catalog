35c828526cfa4b15b955bf751db096a791302e64
Valery Sazonov <vksazonov@gmail.com>
2023-06-11 10:38:27 +0300

    Added Paginagion endpoint

--- src/routers/phones.ts
+++ src/routers/phones.ts
@@ -6,3 +6,4 @@ import * as phonesController from '../controllers/phones';
 export const router = express.Router();
 
 router.get('/', phonesController.getAll);
+router.get('/pagination', phonesController.getByPagination);
--- src/services/phones.ts
+++ src/services/phones.ts
@@ -1,5 +1,24 @@
+/* eslint-disable no-console */
 import { Phone } from '../models/Phone';
 
-export const getAll = () => {
-  return Phone.findAll();
+export const getAll = async() => {
+  const phones = await Phone.findAll();
+
+  return phones;
+};
+
+export const getByPagination = async(page: number, size: number) => {
+  const phoneArray = getAll();
+
+  console.log(phoneArray);
+
+  const requiredPage = page || 1;
+  const requiredSize = size || 10;
+
+  if (requiredPage > 1) {
+    return (await phoneArray)
+      .slice((requiredPage - 1) * requiredSize, requiredPage * requiredSize);
+  }
+
+  return (await phoneArray).slice(0, requiredSize);
 };
