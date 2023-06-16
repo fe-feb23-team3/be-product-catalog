c80c01d2da3bdbc7ea583389328631a7437ec2c3
Nikita Zadorozhnyi <79592253+Zad0rnik@users.noreply.github.com>
2023-06-16 20:22:47 +0300

    fix router (#34)

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ const createServer = () => {
 
   app.use('/phoneCardData', phoneCardDataRouter.router);
   app.use('/phoneCardData/:id', phoneCardDataRouter.router);
-  app.use('/phoneCardData/:id/recomended', phonesRouter.router);
+  app.use('/phoneCardData/:id/recomended', phoneCardDataRouter.router);
   app.use('/phoneCardData/:id/images', phoneCardDataRouter.router);
   app.use('/phoneCardData/:id/images/:orderNumber', phoneCardDataRouter.router);
 
