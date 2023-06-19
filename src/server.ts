9b149c378528cacbe6644f8c04001bb90fdcf9fd
Valery Sazonov <vksazonov@gmail.com>
2023-06-19 16:37:21 +0300

    Deleted params from cors

--- src/server.ts
+++ src/server.ts
@@ -14,12 +14,7 @@ dbInit();
 const createServer = () => {
   const app = express();
 
-  app.use(
-    cors({
-      origin: process.env.CLIENT_URL,
-      credentials: true,
-    }),
-  );
+  app.use(cors());
   app.use(express.json());
 
   app.use('/products/phones', phonesRouter.router);
